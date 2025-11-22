4c05cf57474d0d6a53c451227e230ba5bca8a875
Alexandra-cod9 <00091421@uca.edu.sv>
2025-11-21 23:49:13 -0600

    Update conexion.py

--- SGI-MUERE/config/conexion.py
+++ SGI-MUERE/config/conexion.py
@@ -1,18 +1,19 @@
-import pymysql
 import streamlit as st
+import pymysql
 
 def obtener_conexion():
     try:
         conexion = pymysql.connect(
-            host='127.0.0.1',
-            user='root',
-            password='1234',
-            database='sgi_gapc',
+            host='bhzcn4gxgbe5tcxihqd1-mysql.services.clever-cloud.com',
+            user='usv5pnvafxbrw5hs',
+            password='WiOSztB38WxsKuXjnQgT',
+            database='bhzcn4gxgbe5tcxihqd1',
             port=3306,
             charset='utf8mb4',
-            cursorclass=pymysql.cursors.DictCursor
+            cursorclass=pymysql.cursors.DictCursor,
+            connect_timeout=10
         )
         return conexion
     except Exception as e:
-        st.error(f"❌ Error al conectar a la base de datos: {e}")
+        print(f"Error de conexión: {e}")
         return None
