2a3b91b07c99c336e3b65b9af09257dba131acdd
Alexandra-cod9 <00091421@uca.edu.sv>
2025-11-23 00:03:25 -0600

    Update __init__.py

--- SGI-MUERE/modules/__init__.py
+++ SGI-MUERE/modules/__init__.py
@@ -7,7 +7,7 @@ from .prestamos import mostrar_modulo_prestamos
 from .multas import mostrar_modulo_multas
 from .reportes import mostrar_modulo_reportes
 from .cierre import mostrar_modulo_cierre
-from .info_grupo import mostrar_modulo_info_grupo  # Cambiado de configuracion a info_grupo
+from .info_grupo import mostrar_modulo_info_grupo
 
 __all__ = [
     'mostrar_dashboard_principal',
@@ -18,5 +18,5 @@ __all__ = [
     'mostrar_modulo_multas',
     'mostrar_modulo_reportes',
     'mostrar_modulo_cierre',
-    'mostrar_modulo_info_grupo'  # Cambiado aquí también
+    'mostrar_modulo_info_grupo'
 ]
