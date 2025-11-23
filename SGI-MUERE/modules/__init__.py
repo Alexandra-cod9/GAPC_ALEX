3733cb85436da505f634d4e1d752d9dd5e4f4118
Alexandra-cod9 <00091421@uca.edu.sv>
2025-11-23 00:09:31 -0600

    Update __init__.py

--- SGI-MUERE/modules/__init__.py
+++ SGI-MUERE/modules/__init__.py
@@ -1,22 +1,2 @@
-# Exportar todas las funciones de los módulos
-from .dashboard import mostrar_dashboard_principal
-from .miembros import mostrar_modulo_miembros
-from .reuniones import mostrar_modulo_reuniones
-from .aportes import mostrar_modulo_aportes
-from .prestamos import mostrar_modulo_prestamos
-from .multas import mostrar_modulo_multas
-from .reportes import mostrar_modulo_reportes
-from .cierre import mostrar_modulo_cierre
-from .info_grupo import mostrar_modulo_info_grupo
-
-__all__ = [
-    'mostrar_dashboard_principal',
-    'mostrar_modulo_miembros',
-    'mostrar_modulo_reuniones',
-    'mostrar_modulo_aportes',
-    'mostrar_modulo_prestamos',
-    'mostrar_modulo_multas',
-    'mostrar_modulo_reportes',
-    'mostrar_modulo_cierre',
-    'mostrar_modulo_info_grupo'
-]
+# Módulos del sistema GAPC
+# Este archivo hace que Python trate la carpeta como un paquete
